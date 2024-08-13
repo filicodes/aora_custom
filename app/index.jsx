55e0265ef2bec485c13afdc3bc778ea480ed13fd
filicodes <nimeshadls8@gmail.com>
2024-08-13 14:42:04 +0930

    created the first route

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aora HAHAHA</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        GO to Profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
